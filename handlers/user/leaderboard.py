ef3382ca6b277a1a5844d58a96208b75ca2f3859
pprly <evgepperly@gmail.com>
2025-10-19 20:08:18 -0400

    fix leaderboard

--- handlers/user/leaderboard.py
+++ handlers/user/leaderboard.py
@@ -24,12 +24,12 @@ router = Router()
 
 async def check_user_subscription(bot: Bot, user_id: int) -> bool:
 #     """Проверка подписки пользователя на канал"""
-        return True
-#     try:
-#         member = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=user_id)
-#         return member.status in ["member", "administrator", "creator"]
-#     except Exception as e:
-#         print(f"⚠️ Ошибка проверки подписки для {user_id}: {e}")
+#        return True
+    try:
+        member = await bot.get_chat_member(chat_id=config.CHANNEL_ID, user_id=user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except Exception as e:
+        print(f"⚠️ Ошибка проверки подписки для {user_id}: {e}")
         return False
 
 
