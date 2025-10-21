088cbbaa77f38dd94b2a4c5e44db0eb66f7c31dd
pprly <evgepperly@gmail.com>
2025-10-21 09:08:13 -0400

    fix vite.config.js

--- miniapp/vite.config.js
+++ miniapp/vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/giftbottg/',  // 👈 Название твоего репозитория
+  root: './',
+  publicDir: 'public',
   build: {
     outDir: '../docs',  // 👈 Собираем в корневую папку docs/
     emptyOutDir: false, // Не удаляем rules.html
     sourcemap: false,
     minify: 'terser',
     rollupOptions: {
+      input: resolve(__dirname, 'public/index.html'), // 👈 Явно указываем путь к HTML
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
